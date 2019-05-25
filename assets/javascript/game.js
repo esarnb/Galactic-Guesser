d2da95401e1b45acf7dcd504e3d332990dc158a5
Esar Behlum <esarnb@gmail.com>
2019-05-25 16:56:17 -0700

    Added helper functions to add and check value with counters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,79 @@
 var wins = 0, losses = 0;
 var score = 0, goal = 0;
-var pressed;
-
+var endTheGame = false, pressed;
+/**
+ * To get a range: (random() * max-min+1)+min)
+ */
 function randomGoal() {
-    return (Math.floor(Math.random() * 100) + 21)
+    return (Math.floor(Math.random() * 102) + 19);
 }
 
 function randomValues() {
-    var values = [1], insert=1;
-    for(var i = 1; i < 4; i++) {
-        while(values.includes(insert)) insert = (Math.floor(Math.random() * 12) + 1);
-        values.push(insert);
-    }
-
-    $('.planet').each(function(i, obj) {
-        $(obj).attr("value", values[i]);
-    });
-
-    console.log(values);
-    
+    return (Math.floor(Math.random() * 12) + 1);
 }
 
+function addToScore(inputValue) {
+    var currentScore = parseInt($("#totalScore").text())
+    currentScore += inputValue;
+
+    $("#totalScore").text(currentScore)
+
+    if (currentScore === goal) {
+        wins++;
+        $("#wins").text(`Wins: ${wins}`);
+        endGame(" You Win! c: ");
+    }
+    else if (currentScore > goal) {
+        losses++;
+        $("#losses").text(`Losses: ${losses}`);
+        endGame(" You lose :c ");
+    }
+}
+
+function endGame(endPrompt) {
+    $("#status").text(endPrompt);
+    endTheGame = true;
+
+    setTimeout(() => {
+        reset();
+    }, 5000);
+}
+
+/**
+ * Resets all game values to initial state, 
+ * except for win/loss
+ */
 function reset() {
     //reset score and visual
     score = 0;
-    
-    //Reset end-game prompt
+    $("#totalScore").text(score);  
 
+    //Reset end-game prompt
+    $("#status").text("");
 
     //Assign new random goal number
     goal = randomGoal();
+    $("#numberGoal").text(goal)
 
     //Assign new values to planets
-     randomValues();
+    $('.planet').each(function(i, obj) {
+        $(obj).attr("value", randomValues());
+    });
+
+    //After all is reset, unlock game
+    endTheGame = false;
 }
 
-reset();
 $(document).ready(function() {
+    reset();
     $(".planet").on("click", function(event) {
-        console.log($(this).attr("value"));
+        pressed = parseInt($(this).attr("value"));
+        console.log(pressed);
+        
+        //If the game is not done
+        if (!endTheGame) {
+            addToScore(pressed)
+        }
         
     })
 })
